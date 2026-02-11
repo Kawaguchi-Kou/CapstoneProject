3de201996f573146b683c2eb09eb13a08e828638
haiquan2023 <quannhse182774@fpt.edu.vn>
2026-02-11 10:39:45 +0700

    edit migration, type in AccountSub and AdSubPac

---
+++ Infrastructure/Migrations/20260211033218_EditTypeInAccSubAndAdSubPac.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditTypeInAccSubAndAdSubPac : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ad_subscription_packages",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            //migrationBuilder.AlterColumn<double>(
+            //    name: "MaxAdsPerPeriod",
+            //    table: "ad_subscription_packages",
+            //    type: "double precision",
+            //    nullable: false,
+            //    oldClrType: typeof(string),
+            //    oldType: "text");
+
+            migrationBuilder.Sql(
+                @"ALTER TABLE ad_subscription_packages 
+                  ALTER COLUMN ""MaxAdsPerPeriod"" 
+                  TYPE double precision 
+                  USING ""MaxAdsPerPeriod""::double precision;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "ad_subscription_packages",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaxAdsPerPeriod",
+                table: "ad_subscription_packages",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
