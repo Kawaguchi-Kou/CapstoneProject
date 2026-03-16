4287fb1b9456fb7356ccdb1363ac020dd5a5db27
Trungkien13 <kientdtse183827@fpt.edu.vn>
2026-03-16 09:42:01 +0700

    khắc phục account

---
+++ Application/Services/AdminService.cs
@@ -0,0 +1,54 @@
+﻿using Application.DTOs.Requests;
+using Application.DTOs.Responses;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AdminService(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        public async Task<List<Account>> GetAll()
+        {
+            var accounts = await _authRepository.GetAllAccountsAsync();
+
+            return accounts;
+        }
+        public async Task ActivateAccount(Guid id)
+        {
+            var account = await _authRepository.GetByIdAsync(id);
+
+            if (account == null)
+                throw new Exception("Account not found");
+
+            account.IsActive = true;
+
+            await _authRepository.SaveChangesAsync();
+        }
+
+        public async Task DeactivateAccount(Guid id)
+        {
+            var account = await _authRepository.GetByIdAsync(id);
+
+            if (account == null)
+                throw new Exception("Account not found");
+
+            account.IsActive = false;
+
+            await _authRepository.SaveChangesAsync();
+        }
+    }
+}
